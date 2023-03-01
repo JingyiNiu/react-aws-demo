3f6a6ba210192469cfa993852f7470acada6fc8b
JingyiNiu <niu.jy1203@gmail.com>
2023-03-01 15:53:18 +1300

    update server api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
-const PORT = 8000;
-
 const app = express();
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../client', 'build')));
+// app.use(express.static(path.join(__dirname, '../client', 'build')));
+app.get('/api/posts', async (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: [
+            {
+                id: 1,
+                title: 'Hi',
+            },
+            {
+                id: 2,
+                title: 'Hello',
+            },
+        ],
+    });
+});
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
